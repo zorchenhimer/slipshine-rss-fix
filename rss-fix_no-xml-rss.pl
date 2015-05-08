4c3586d5a1b0d6b4c306865f8586200282510090
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-05-07 20:59:51 -0400

    Simplified everything and split single items with multiple links into their own items.

--- rss-fix_no-xml-rss.pl
+++ rss-fix_no-xml-rss.pl
@@ -15,35 +15,29 @@ $header_sections{'docs'} = '';
 $header_sections{'generator'} = 'Not Slipshine.  That shit\'s borked.';
 
 my @items = $borked_rss =~ /<item>(.*?)<\/item>/smg;
-
-my @new_items;
+my @ul_links;
 
 foreach my $item (@items) {
     my %sections = $item =~ /<([^>]+)>(.*?)<\/\1>/smg;
 
-    $sections{'description'} =~ s/^\s+//sg;
-    $sections{'description'} =~ s/\s+/ /sg;
-    $sections{'description'} =~ s/<!\[CDATA\[//sg;
-    $sections{'description'} =~ s/\s*\]\]>//sg;
+    $sections{'description'} =~ m/<ul>(.*?)<\/ul>/smg;
+    my @list_items = split /<li>/, $1;
+    foreach my $l (@list_items) {
+        if ($l) {
+            $l =~ m#<a href="(.*?)">(.*?)</a>#;
+            my $ul_link = $1;
+            my $ul_title = $2;
 
-    $sections{'description'} =~ /href="([^"]+)"/;
-    my $real_link = $1;
+            my %link_data;
+            $link_data{'link'} = $ul_link;
+            $link_data{'title'} = $ul_title;
+            $link_data{'pubDate'} = $sections{'pubDate'};
 
-    $sections{'description'} =~ s/<br> /\n/g;
-    $sections{'description'} =~ s/<[^>]+>//g;
-    $sections{'description'} =~ s/\n/ - /sg;
-    $sections{'description'} =~ s/\s+$//sg;
-
-    $sections{'description'} =~ /^(.+) by (.+) - (.+)$/;
-    my $real_title = $1. ' - '. $3;
-
-    ## Re-assignment
-    $sections{'link'} = $real_link;
-    $sections{'title'} = $real_title;
-    $sections{'guid'} = md5_hex($real_title.$real_link);
-    $sections{'description'} .= "<br /><a href=\"$real_link\">$real_link</a>";
-    push(@new_items, \%sections);
+            push @ul_links, \%link_data;
+        }
+    }
 }
+
 print "Content-type: text/xml\n\n";
 
 print qq(<?xml version="1.0" encoding="iso-8859-1" ?>\n<rss version="2.0">\n);
@@ -53,17 +47,13 @@ print "<channel>\n\t<title>$header_sections{title}</title>\n",
     "\t<generator>$header_sections{generator}</generator>\n",
     "\t<link>$header_sections{link}</link>\n";
 
-foreach my $n_item (@new_items) {
+foreach my $nlink (@ul_links) {
     print "\t<item>\n";
-    foreach my $section (sort keys %{$n_item}) {
-        if( $section eq 'description' ) {
-            print "\t\t<description><![CDATA[$n_item->{description}]]></description>\n";
-        } elsif( $section eq 'guid' ) {
-            print "\t\t<guid isPermaLink=\"false\">$n_item->{guid}</guid>\n";
-        } else {
-            print "\t\t<$section>$n_item->{$section}</$section>\n";
-        }
-    }
+    print "\t\t<title>$nlink->{title}</title>\n";
+    print "\t\t<guid>$nlink->{link}</guid>\n";
+    print "\t\t<link>$nlink->{link}</link>\n";
+    print "\t\t<pubDate>$nlink->{pubDate}</pubDate>\n";
+    print "\t\t<description><![CDATA[<a href=\"$nlink->{link}\">$nlink->{title}</a>]]></description>\n";
     print "\t</item>\n";
 }
 print "</channel>\n</rss>\n";
